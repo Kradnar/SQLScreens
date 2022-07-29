efd880817410a09535079383623d6690274fc2f2
Kradnar <pete.daniel.1975@gmail.com>
2022-07-29 11:48:22 +0100

    ver1.5 - Update updated and User feedback on Delete(and Update)

--- src/movie/functions.js
+++ src/movie/functions.js
@@ -22,7 +22,7 @@ exports.readMovie = async (movieObj) => {
 
 exports.filterMovie = async (movieObj) => {
   try{
-    const results = await Movie.findAll({where:{title: movieObj.title}});
+    const results = await Movie.findAll({where:{title: movieObj.title, actor:movieObj.actor}});
     console.log(results);
   }
   catch(error) {
@@ -32,8 +32,20 @@ exports.filterMovie = async (movieObj) => {
 
 exports.updateMovie = async (movieObj) => {
   try{
-    await Movie.update({title: movieObj.newTitle, actor: movieObj.newActor}, 
-      {where:{title: movieObj.title, actor: movieObj.actor}});
+    if (movieObj.title) {
+      await Movie.update({title: movieObj.newTitle}, {where:{title: movieObj.title}});
+      console.log(`The Movie ${movieObj.title} will now be called ${movieObj.newTitle} which is a far better name`)
+      console.log('Marketing is now updating all promotional material...')
+    }
+    else if (movieObj.actor) {
+      await Movie.update({actor: movieObj.newActor}, {where:{actor: movieObj.actor}});
+      console.log(`${movieObj.actor} has been fired, ${movieObj.newActor} is now in the role`)
+      console.log('HR is now processing the change...')
+    }
+    else {
+      console.log("Please enter either --title and --newTitle or")
+      console.log("--actor and --newActor")
+    }
   }
   catch(error) {
     console.log(error);
@@ -42,7 +54,9 @@ exports.updateMovie = async (movieObj) => {
 
 exports.deleteMovie = async (movieObj) => {
   try{
+    // let title = movieObj.title
     await Movie.destroy({where:{title: movieObj.title}});
+    console.log(`${movieObj.title} has been destroyed!  Wiped of the face of the...   Database`)
   }
   catch(error) {
     console.log(error);
